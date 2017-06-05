906d4dd843ef86f7ca860ba73c1cc65c6ece72ef
cgjohnsonli <richardsilkli@outlook.com>
2017-06-05 14:31:09 -0400

    6.5 update
    create lists of photos of projects,
    change color
    refine text

--- noa.js
+++ noa.js
@@ -2,10 +2,27 @@
 
 $(function() {
 	var array = [
-	"bg/hutong.jpg",
-	"bg/trail.jpg",
-	"bg/courtyard.jpg",
-	"bg/firstavenue.jpg",
+	"bg/hutong/hutong0.jpg",
+	"bg/trail/trail0.jpg",
+	"bg/courtyard/courtyard0.jpg",
+	"bg/firstavenue/firstavenue0.jpg",
+	"bg/klekovaca/klekovaca0.jpg",
+	"bg/unplaza/unplaza0.jpg",
+	"bg/ninthavenue/ninthavenue0.jpg",
+	"bg/flower/flower0.jpg",
+	"bg/hotel-hester/hotel-hester0.jpg",
+	"bg/bicity/bicity0.jpg",
+	"bg/cluster/cluster0.jpg",
+	"bg/courtyardhousing/courtyardhousing0.jpg",
+	"bg/terrace/terrace0.jpg",
+	"bg/science-city/science-city0.jpg",
+	"bg/trail/trail0.jpg",
+	"bg/west-hotel/west-hotel0.jpg",
+	"bg/science-island/science-island0.jpg",
+	"bg/museo-lima/museo-lima0.jpg",
+	"bg/allegheny/allegheny0.jpg",
+	"bg/dorigny/dorigny0.jpg",
+	"bg/helsinki/helsinki0.jpg",
 	];
 	var l = array.length;
 	var r = Math.floor(Math.random()*l);
@@ -21,10 +38,26 @@ $(function() {
 });
 
 
-
+/*
 $(function(){
 	$('#top, #top1').click(function(){
 	var bglink = $(this).attr("href");
+	$("div#randomimg2").css("background-image","url("+bglink+")");
+	 return false;
+	});
+});
+*/
+
+$(function(){
+	var count = 0;
+	$('#top, #top1').click(function(){
+	var bglink = $(this).attr("href");
+	var use = bglink.replace(/[^0-9]/ig,"");
+	var number = use + 1;
+	count = count + 1;
+	if(count > use){count = 0;}
+	//count = count%number;
+	bglink = bglink.replace(use,count);
 	$("div#randomimg2").css("background-image","url("+bglink+")");
 	 return false;
 	});
